f8fe6071dcef20083e99a43382d6a540c2741149
despotiw <shannyuwima@proton.me>
2025-02-04 16:00:40 +0300

    Update Events.jsx

--- pages/Events.jsx
+++ pages/Events.jsx
@@ -1,1 +1,18 @@
 
+// src/pages/Events.jsx
+import React from 'react';
+import '../styles/Events.css';
+import EventCalendar from '../components/EventCalendar';
+import TicketPurchase from '../components/TicketPurchase';
+
+const Events = () => {
+  return (
+    <div className="events-page">
+      <h2>Events & Festivals</h2>
+      <EventCalendar />
+      <TicketPurchase />
+    </div>
+  );
+};
+
+export default Events;
