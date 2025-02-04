74b4f3428d6f78a2bb83219ebb9badf985ac9a5d
despotiw <shannyuwima@proton.me>
2025-02-04 15:44:15 +0300

    Update ARNavigation.jsx

--- pages/ARNavigation.jsx
+++ pages/ARNavigation.jsx
@@ -1,1 +1,18 @@
 
+// src/pages/ARNavigation.jsx
+import React from 'react';
+import '../styles/ARNavigation.css';
+import ARViewer from '../components/ARViewer';
+import QRScanner from '../components/QRScanner';
+
+const ARNavigation = () => {
+  return (
+    <div className="ar-navigation-page">
+      <h2>AR Navigation</h2>
+      <ARViewer />
+      <QRScanner />
+    </div>
+  );
+};
+
+export default ARNavigation;
